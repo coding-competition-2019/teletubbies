e9a6280187eb62318ab7c9302b9cc69b8710f9f2
milaguyenlam <44195479+milaguyenlam@users.noreply.github.com>
2019-12-04 10:36:16 +0100

    Update data_transformer + template for Daniel

--- backend/data_transformer.py
+++ backend/data_transformer.py
@@ -1,7 +1,31 @@
 import json
 
+#street passed as string
+def get_coordinates(street):
+    coordinate_X = 0
+    coordinate_Y = 0
+
+    #assign float values into coordinate_X and coordinate_Y - thats all
+    return coordinate_X, coordinate_Y
+
+
+
 with open("places.json") as f:
     data = json.load(f)
 
+print(type(data))
+print(type(data["places"]))
+print(type(data["places"][0]))
+
 for place in data["places"]:
+    address = place["address"]
+    print(type(address))
+    street = address["street"]
+    coordinate_X, coordinate_Y = get_coordinates(street)
+    place["coordinate_X"] = coordinate_X
+    place["coordinate_Y"] = coordinate_Y
+
+with open("places_transformed.json", "w") as f:
+    print(json.dump(data, indent=2))
+    json.dump(data, f)
 
