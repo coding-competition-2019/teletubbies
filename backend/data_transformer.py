b04a61d238f62744292542828581385b773e6e9c
DanielSmetanka <37157000+DanielSmetanka@users.noreply.github.com>
2019-12-04 11:05:51 +0100

    Update data_transformer.py
    Debug

--- backend/data_transformer.py
+++ backend/data_transformer.py
@@ -3,22 +3,27 @@ import json, googlemaps
 #street passed as string
 def get_coordinates(street):
     
+    coordinate_X = 0
+    coordinate_Y = 0
+    
     gmaps_key = googlemaps.Client(key = "AIzaSyAVsQFt2JPrScp3gzgvu4FwnCGxH8mvmpw")
 
     geocode_res = gmaps_key.geocode(street)
-    coordinate_X = geocode_res[0]["geometry"]["location"]["lat"]
-    coordinate_Y = geocode_res[0]["geometry"]["location"]["lng"]
     
-    coordinate_X = 0
-    coordinate_Y = 0
+    try:
+        coordinate_X = geocode_res[0]["geometry"]["location"]["lat"]
+        coordinate_Y = geocode_res[0]["geometry"]["location"]["lng"]
+    except:
+        coordinate_X = None
+        coordinate_Y = None
 
     #assign float values into coordinate_X and coordinate_Y - thats all
     return coordinate_X, coordinate_Y
 
 
 
-with open("places.json", "r", encoding="utf8") as f:
-    data = json.loads(f.read())
+with open("places.json") as f:
+    data = json.load(f)
 
 print(type(data))
 print(type(data["places"]))
@@ -26,11 +31,13 @@ print(type(data["places"][0]))
 
 for place in data["places"]:
     address = place["address"]
+    print(type(address))
     street = address["street"]
     coordinate_X, coordinate_Y = get_coordinates(street)
     place["coordinate_X"] = coordinate_X
     place["coordinate_Y"] = coordinate_Y
 
-with open("places_transformed.json", "w", encoding="utf8") as f:
-    json.dump(data, f, indent=2)
+with open("places_transformed.json", "w") as f:
+    print(json.dump(data, indent=2))
+    json.dump(data, f)
 
