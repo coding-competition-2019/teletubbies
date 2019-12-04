cc007a3d16a030738277c23ad4158e2bf0d8bd48
Jindřich Drobný <jindrich.drobny@gmail.com>
2019-12-04 16:20:12 +0100

    isloggedin

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
 export class AuthService {
 
   public get IsLoggedIn(): Observable<boolean> {
-    return of(true);
+    return this.http.post('http://127.0.0.1:5000/login', {}).pipe(map((x: any) => x.success === 0));
   }
 
   constructor(private http: HttpClient) { }
